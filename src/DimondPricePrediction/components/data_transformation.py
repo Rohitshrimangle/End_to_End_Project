ae43145fe9ecad0b040a05820f821293f141038d
Rohit Shrimangle <Rohitshrimangle@431513gmail.com>
2024-05-26 10:06:03 +0530

    added model training file

--- src/DimondPricePrediction/components/data_transformation.py
+++ src/DimondPricePrediction/components/data_transformation.py
@@ -1,10 +1,101 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+
+from dataclasses import dataclass
+from src.DimondPricePrediction import customexception
+from src.DimondPricePrediction.logger import logging
+
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OrdinalEncoder,StandardScaler
+
+from src.DimondPricePrediction.utils.utlis import save_object
 
 
+class DataTransformationConfig:
+    preprocessor_obj_file_path=os.path.join('artifaccts','preprocessor.pk1')
 
 class DataTransformation:
     def __init__(self):
-        pass
+        self._data_transformation_config=DataTransformationConfig()
+
 
     def get_data_transformation(self):
-        pass
-    
+        
+        try:
+            logging.info('Data Transformation initiated')
+
+            categorical_cols = ['cut', 'color', 'clarity']
+            numerical_cols = ['carat', 'depth','table', 'x','y','z']
+            
+            
+            cut_categories = ['Premium', 'Very Good', 'Ideal', 'Good', 'Fair']
+            color_categories =['F', 'J', 'G', 'E', 'D', 'H', 'I']
+            clarity_categories =['VS2', 'SI2', 'VS1', 'SI1', 'IF', 'VVS2', 'VVS1', 'I1']
+
+            logging.info("Pipeline Initiated")
+
+
+            num_pipeline=Pipeline(
+                steps=[
+                    ('imputer',SimpleImputer(strategy='median')),
+                    ('scaler',StandardScaler())
+                ]
+            )
+
+            cat_pipeline=Pipeline(
+                steps=[
+                    ("impter",SimpleImputer(strategy='most_frequent')),
+                    ('ordinalencoder',OrdinalEncoder(categories=[cut_categories,color_categories,clarity_categories]))
+                    ('scaler',StandardScaler())
+                    ]
+            )
+
+
+            preprocessor=CloumnTransformer([
+                ('num_pipline',num_pipline,numerical_cols),
+                ('cat_pipline',cat_pipeline,categorical_cols)
+            ])
+
+            return preprocessor
+
+        except Exception as e:
+            logging.info("Exception occured in the initiate_datatransformation")
+
+            raise customexception(e,sys)
+
+    def initialize_data_transformation(self,train_path,test_path):
+        try:
+            train_df=pd.read_csv(train_path)
+            test_df=pd.read_csv(test_path)
+
+            logging.info("read train and test data complete")
+            logging.info(f"Train Dataframe Head:\n{train_df.head().to_string()}")
+            logging.info(f"Test Dataframe Head:\n{test_df.head().to_string()}")
+
+            preprocessing_obj = self.get_data_transformation()
+
+            target_column_name ='price'
+            drop_columns = [target_column_name,id]
+
+            input_feature_train_df = train_df.drop(columns=drop_columns,axis=1)
+            target_feature_train_df=train_df[target_column_name]
+
+            input_feature_train_arr = preprocessing_obj.fit_transformation(input_feature_train_df)
+
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
+
+            logging.info('applying preprocessor object on training and testing datasets.')
+
+            save_object(
+                self.data_transformation_config.preprocessor_obj_file_path,
+                obj = preprocessing_obj
+            )
+
+        except Exception as e:
+            logging.info("Exception occured in the initiate_datatransformation")
+
+            raise customexception(e,sys)
